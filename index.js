8512ccb19d49bf377b161fb5dccb8d00753af414
Philip Simpson <pwsjas433@hotmail.com>
2022-07-07 22:50:56 +0000

    eslint fixes and removing undeeded code

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
     title: title,
     description: description,
@@ -20,16 +13,8 @@ function newTask(title, description) {
     }
   };
   return task;
-}
-
-
+};
 // DRIVER CODE BELOW
-
-// newTask("Clean Cat Litter"); // task 0
-// newTask("Do Laundry"); // task 1
-// logTaskState(0); // Clean Cat Litter has not been completed
-// completeTask(0);
-// logTaskState(0); // Clean Cat Litter has been completed
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
